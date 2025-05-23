16ff3050a17c0a04149010b63ea72c16d9778cfb
EO <504068014080@tutanota.com>
2025-05-23 15:43:47 +0900

    qq

--- scripts/build_zola.sh
+++ scripts/build_zola.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 set -e
 
-# Specify the exact Zola version and filename
-ZOLA_VERSION="0.19.1"
-ZOLA_FILENAME="zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+# Download Zola from your provided URL
+curl -sSL "https://github.com/getzola/zola/releases/download/v0.19.1/zola-v0.19.1-x86_64-unknown-linux-gnu.tar.gz" -o zola.tar.gz
 
-# Download the release tarball from GitHub
-curl -sSL "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/${ZOLA_FILENAME}" -o zola.tar.gz
-
-# Extract and build
+# Extract and run Zola build
 tar -xzf zola.tar.gz
 ./zola build
