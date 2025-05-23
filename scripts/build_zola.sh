96ce5b516f134bbe6c941c112288219dc8c97cfc
EO <504068014080@tutanota.com>
2025-05-23 15:40:29 +0900

    Use Zola v0.19.1 in build script

--- scripts/build_zola.sh
+++ scripts/build_zola.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-# Download Zola binary
-curl -sSfL https://github.com/getzola/zola/releases/latest/download/zola-x86_64-unknown-linux-gnu.tar.gz | tar xz
-# Run zola build
-./zola build
+set -e
 
+# Set Zola version and target archive
+ZOLA_VERSION="0.19.1"
+ZOLA_ARCHIVE="zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+
+# Download and extract Zola
+curl -sSL "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/${ZOLA_ARCHIVE}" -o zola.tar.gz
+tar -xzf zola.tar.gz
+
+# Run the build
+./zola build
